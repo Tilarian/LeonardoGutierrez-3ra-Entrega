44458a0708b49adc28bdb99901cedac21c125d06
Leonardo Gutierrez <tilarian@outlook.com>
2024-06-13 20:34:57 -0300

    Se hacen arreglos varios

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,12 +3,7 @@ from inicio import views
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
-    path('template1/<str:nombre>/<str:apellido>/<int:edad>', views.template1),
-    path('template2/<str:nombre>/<str:apellido>/<int:edad>', views.template2),
-    path('template3/<str:nombre>/<str:apellido>/<int:edad>', views.template3),
-    path('template4/<str:nombre>/<str:apellido>/<int:edad>', views.template4),
     path('probando/', views.probando, name="probando"),
-    #path('usuarios/crear/<str:nombre>/<str:apellido>', views.crear_usuario, name="crear_usuario")
     path('usuarios/crear/', views.crear_usuario_v2, name="crear_usuario_v2"),
     path('usuarios/', views.usuarios, name="usuarios")
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render, redirect
-from datetime import datetime
-
-from django.http import HttpResponse
-from django.template import Template, Context, loader
 
 from inicio.models import Usuario
 
@@ -14,67 +10,6 @@ def inicio(request):
    # return HttpResponse('Bienvenidos!!')
    return render (request, 'inicio/index.html')
 
-def template1(request, nombre, apellido, edad):
-    fecha = datetime.now()
-    suma = 4 + edad
-    
-    return HttpResponse (f'<h1>Mi template1</h1> -- fecha: {fecha} -- Buenas {nombre} {apellido} {edad}')
-
-def template2(request, nombre, apellido, edad):
-    
-    archivo_abierto = open(r'C:\Users\Tila\Desktop\Proyecto\templates\template2.html')
-    # archivo_abierto = open('templates\template2.html')
-    
-    template = Template(archivo_abierto.read())
-    
-    archivo_abierto.close()
-    fecha = datetime.now()
-    
-    datos = {
-        'fecha': fecha,
-        'nombre': nombre,
-        'apellido':apellido,
-        'edad': edad,
-        }
-    
-    contexto = Context(datos)
-    
-    template_renderizado = template.render(contexto)
-    
-    return HttpResponse(template_renderizado)
-
-
-def template3(request, nombre, apellido, edad):
-
-    template = loader.get_template('template3.html')
-    
-    fecha = datetime.now()
-    
-    datos = {
-        'fecha': fecha,
-        'nombre': nombre,
-        'apellido':apellido,
-        'edad': edad,
-        }
-    
-    template_renderizado = template.render(datos)
-    
-    return HttpResponse(template_renderizado)
-
-
-def template4(request, nombre, apellido, edad):
-
-    fecha = datetime.now()
-    
-    datos = {
-        'fecha': fecha,
-        'nombre': nombre,
-        'apellido':apellido,
-        'edad': edad,
-        }
-
-    return render(request, 'template2.html', datos)
-
 def probando (request):
     
     lista = list(range(500))
@@ -83,25 +18,7 @@ def probando (request):
     print(numeros)
     return render(request, 'probando_if_for.html', {'numeros': numeros})
 
-def crear_usuario(request, nombre, apellido):
-    usuario = Usuario(nombre=nombre, apellido=apellido)
-    usuario.save()
-    return render(request, 'usuario_templates/creacion.html', {"usuario": usuario})
-
 def crear_usuario_v2(request):
-    
-    #V1
-    #print('valor de la request:', request)
-    #print('valor de GET:', request.GET)
-    #print('valor de POST:', request.POST)
-    
-    #if request.method == 'POST':
-    #    usuario = Usuario(nombre=request.POST.get('nombre'), apellido=request.POST.get('apellido'), correo=request.POST.get('correo'), sugerencia=request.POST.get('sugerencia'))
-    #    usuario.save()
-    
-    #return render(request, 'inicio/crear_usuario_v2.html')
-    
-    #V2
     print('valor de la request:', request)
     print('valor de GET:', request.GET)
     print('valor de POST:', request.POST)
