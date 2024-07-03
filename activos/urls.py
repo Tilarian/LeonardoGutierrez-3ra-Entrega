6a5ea785cdb9e0490456eef6d109150f38dc1b20
Leonardo Gutierrez <tilarian@outlook.com>
2024-07-03 12:42:21 -0300

    Se agrega botones de editar y eliminar para los users

--- activos/urls.py
+++ activos/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('users/', views.Users.as_view(), name='users'),
     path('users/create/', views.CreateUser.as_view(), name='create_user'),
-    path('users/<int:pk>/', views.DisplayUser.as_view(), name='display_user')
+    path('users/<int:pk>/', views.DisplayUser.as_view(), name='display_user'),
+    path('users/<int:pk>/edit/', views.EditUser.as_view(), name='edit_user'),
+    path('users/<int:pk>/delete/', views.DeleteUser.as_view(), name='delete_user')
 ]
--- activos/views.py
+++ activos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import User
 from django.urls import reverse_lazy
@@ -18,8 +18,16 @@ class CreateUser(CreateView):
     fields = ['name','surname','email','age']
     
 class EditUser(UpdateView):
-    ...
-    
+    model = User
+    template_name = 'users/edit_user.html'
+    success_url = reverse_lazy('users')
+    fields = ['name','surname','email','age']
+
 class DisplayUser(DetailView):
     model = User
     template_name = 'users/display_user.html'
+    
+class DeleteUser(DeleteView):
+    model = User
+    template_name = 'users/delete_user.html'
+    success_url = reverse_lazy('users')
